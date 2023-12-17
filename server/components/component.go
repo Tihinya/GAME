c8a96bbb7545ff7c13815b1ad9a439c366a4adb3
TiHinya <st.tihinya@gmail.com>
2023-12-17 13:44:12 +0200

    bbb

---
+++ server/components/component.go
@@ -0,0 +1,43 @@
+package components
+
+import "time"
+
+type PositionComponent struct {
+	X, Y float64
+}
+type Vec2 struct {
+	X, Y int
+}
+
+type MotionComponent struct {
+	Velocity     Vec2
+	Acceleration Vec2
+}
+
+type SpriteComponent struct {
+	Texture string
+}
+
+type CollisionComponent struct {
+}
+
+type HealthComponent struct {
+	CurrentHealth int
+	MaxHealth     int
+}
+
+type InputComponent struct {
+	Input map[string]bool
+}
+
+type TimerComponent struct {
+	Time time.Time
+}
+
+type PowerUpComponent struct {
+}
+
+type DamageComponent struct {
+	DamageAmount int
+	DamageType   string
+}

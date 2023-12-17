b594f656370c89834bf2863d84e4487a7836fa21
mvahe <martinvahe@hotmail.com>
2023-12-18 01:33:12 +0200

    State manager added

---
+++ server/game/statemanager/statemanager.go
@@ -0,0 +1,33 @@
+package statemanager
+
+type GameState interface {
+	Init()
+	Update(dt float64)
+	Render()
+	Cleanup()
+}
+
+type StateManager struct {
+	currentState GameState
+	states       map[string]GameState
+}
+
+func (sm *StateManager) SetState(name string) {
+	if sm.currentState != nil {
+		sm.currentState.Cleanup()
+	}
+	sm.currentState = sm.states[name]
+	sm.currentState.Init()
+}
+
+func (sm *StateManager) Update(dt float64) {
+	if sm.currentState != nil {
+		sm.currentState.Update(dt)
+	}
+}
+
+func (sm *StateManager) Render() {
+	if sm.currentState != nil {
+		sm.currentState.Render()
+	}
+}
